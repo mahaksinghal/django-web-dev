c2b20856d4c577ebf0075b1dd1d82d570dd91adc
mahak <mahaksinghal567@gmail.com>
2024-07-25 16:13:15 +0530

    js

---
+++ index.js
@@ -0,0 +1,26 @@
+// node --watch file_name
+// node file_name
+
+console.log("My first javascript program")
+
+num = 3456;
+console.log(num)
+console.log(typeof num)
+
+num1 = 34.56;
+console.log(num1)
+console.log(typeof num1)
+
+name = "Mahak"
+console.log(name)
+console.log(typeof name)
+
+num2 = 12
+num3 = 34
+console.log(num2 + num3)
+
+num4 = "94"
+console.log(num2 + num4)
+
+console.log(1+'1'+1);
+
